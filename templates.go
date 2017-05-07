350eeed12de8e46dee7baa8f001321905b642fcb
Gideon <celtictarot386@gmail.com>
2017-05-07 17:15:28 -0400

    continued with adding more html

--- templates.go
+++ templates.go
@@ -1,12 +1,21 @@
 package main
 
+/*HTML meaning guide for my sanity:
+<br>: page break
+<html> and </html>: beginning and end of html section
+<body> and </body>: main part with text
+*/
 var (
 	firstPage = `<html>
 <head><title>This is the first page</title></head>
 <body>
-There is text here.
+There is text here. <br>
 
 and more text here.
+
+
 </body>
 </html>`
 )
+
+//<img src="./testingpics/Graphic1.jpg" alt="Testing Graphic" style="height:500;width:500">
