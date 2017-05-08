5ef283eb29977ca6f76bfc4e1d859fcf82cc1db7
Gideon <celtictarot386@gmail.com>
2017-05-07 21:12:38 -0400

    Added more pages! close to getting image pages!!!

--- templates.go
+++ templates.go
@@ -13,9 +13,37 @@ There is text here. <br>
 
 and more text here.
 
+<p>
+field from request: {{ .Fn}}
+</p>
+
+<form action="/app/test/" method="POST">
+  First name: <input type="text" name="fn"><br>
+  Last name: <input type="text" name="tn"><br>
+  <input type="submit" value="Go!">
+</form>
 
 </body>
 </html>`
 )
 
 //<img src="./testingpics/Graphic1.jpg" alt="Testing Graphic" style="height:500;width:500">
+
+var (
+	testPage = `<html>
+  <head><title> This is the second test page!!! </title> </head>
+  <body>
+  testing text<br>
+  and moar testing text!!!<br>
+  And a testing graphic!!!<img src="./testingpics/Graphic1.jpg" alt="Testing Graphic" style="height:500;width:500">
+
+  <p>
+  field from request: {{ .Tn}}
+  </p>
+
+  <form action="/app/main/" method="POST">
+    <input type="submit" value="back to main!">
+  </form>
+
+  </body> </html>`
+)
