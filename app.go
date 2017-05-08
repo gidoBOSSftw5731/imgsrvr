b27ee67d6d2da2c7840b50223b1c77cbba19a174
Gideon <celtictarot386@gmail.com>
2017-05-07 21:13:15 -0400

    Made more pages and made it easier to add moar pages... MOAR!!!

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"net"
 	"net/http"
 	"net/http/fcgi"
@@ -11,38 +12,65 @@ import (
 )
 
 type FastCGIServer struct{}
+type tData struct {
+	Fn string
+	Tn string
+}
+
+func appPage(resp http.ResponseWriter, req *http.Request) {
+	//First page Stuff!
+	firstPageTemplate := template.New("first page templated.")
+	firstPageTemplate, err := firstPageTemplate.Parse(firstPage)
+	if err != nil {
+		fmt.Printf("Failed to parse template: %v", err)
+	}
+	field := req.FormValue("fn")
+	fmt.Println(field)
+	tData := tData{
+		Fn: field,
+	}
+	if err = firstPageTemplate.Execute(resp, tData); err != nil {
+		fmt.Printf("template execute error: %v", err)
+	}
+}
+
+func testingPage(resp http.ResponseWriter, req *http.Request) {
+	//testingPage!!!
+	testPageTemplate := template.New("test page templated.")
+	testPageTemplate, err := testPageTemplate.Parse(testPage)
+	if err != nil {
+		fmt.Printf("Failed to parse template: %v", err)
+	}
+	field := req.FormValue("tn")
+	fmt.Println(field)
+	tData := tData{
+		Tn: field,
+	}
+	if err = testPageTemplate.Execute(resp, tData); err != nil {
+		fmt.Printf("template execute error: %v", err)
+	}
+}
 
 func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	fmt.Println("the req arrived")
 	if req.Body == nil {
 		return
 	}
-	/*
-		body, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			fmt.Printf("Read Body ERROR: %v\n", err)
-		}
-		req.Body.Close()
-	*/
-	//Getting stuff from a GET!! (or to normies, putting together stuff to send)
-	// fieldValue := req.FormValue("field_name")
-	// ret := fmt.Sprintf("<h1>Hello, 世界</h1>\n<p>Behold my Go web app.</p> %s", fieldValue)
-	ret := firstPage
-	/*
-		type UserInput struct {
-			SomeField    string
-			AnotherField int
-			LastOne      string
-		}
-		var inp UserInput
-		json.Unmarshal(body, &inp)
-		//SomeField := "test"
-		resp.Write([]byte(inp.SomeField))
-	*/
-	// now write out data!
-	resp.Write([]byte(ret))
+	fmt.Printf("URL is: %v\n", req.URL.Path)
+
+	switch req.URL.Path {
+	default:
+		appPage(resp, req)
+	/*case "/app/main/":
+	testingPage(resp, req)*/
+	case "/app/test/":
+		testingPage(resp, req)
+	case "app/test"
+		testingPage(resp,req)
+	}
 }
 
+//When everything gets set up, all page setup above this
 func main() {
 	fmt.Println("Starting the program.")
 	listener, _ := net.Listen("tcp", "127.0.0.1:9001")
