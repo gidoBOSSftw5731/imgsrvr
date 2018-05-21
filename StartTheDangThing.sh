316d5f89e24c1c086f77d80b1f840cdc60989400
BuildTools <unconfigured@null.spigotmc.org>
2018-05-21 00:50:04 +0000

    added config to start script

--- StartTheDangThing.sh
+++ StartTheDangThing.sh
@@ -1,1 +1,1 @@
-go run app.go templates.go #This is the command, I made a sh so I dont dont forget the 2 files ='/
+go run app.go templates.go config.go #This is the command, I made a sh so I dont dont forget the 2 files ='/
