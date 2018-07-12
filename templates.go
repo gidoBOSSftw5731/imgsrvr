b070f4443bb0c938d0aa77f4132830915e386145
BuildTools <unconfigured@null.spigotmc.org>
2018-07-12 06:15:22 +0000

    added good title

--- templates.go
+++ templates.go
@@ -11,7 +11,7 @@ var (
 
 <head>
 <meta name="google-site-verification" content="2QLKtDFPPQwFab4Tx2Gf0TJ1SVMI1lSA4VfKsA90SaY" /> //ssshhh...
-<title>This is the first page</title></head>
+<title>Imagen Dot Click</title></head>
 <body>
 <b> EVERYTHING IS EXPALINED IN SOME DETAIL AT MY GITHUB: </b><br>
 https://github.com/gidoBOSSftw5731/imgsrvr/tree/master
